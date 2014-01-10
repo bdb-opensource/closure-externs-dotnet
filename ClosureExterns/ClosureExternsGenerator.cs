0410fa021f9757191a429e48b2657f6f5da8537a
Noam Lewis <lenoam+BDB@gmail.com>
2014-01-09 22:12:21 -0500

    initial

---
+++ ClosureExterns/ClosureExternsGenerator.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using QuickGraph.Algorithms;
+
+namespace BDBPayroll.Billing.API.Web.JSCodeGenerator
+{
+    public class ClosureExternsGenerator
+    {
+        protected readonly Assembly[] _assemblies;
+        protected readonly Type[] _types;
+        protected readonly ClosureExternsOptions _options;
+
+        protected ClosureExternsGenerator(IEnumerable<Type> types, ClosureExternsOptions options)
+        {
+            this._options = options;
+            this._types = types.ToArray();
+            this._assemblies = this._types.GroupBy(x => x.Assembly)
+                                          .Select(x => x.Key)
+                                          .ToArray();
+        }
+
+        #region Public Methods
+
+        /// <summary>
+        /// Generates closure externs using the given options.
+        /// </summary>
+        /// <param name="types">A set of base types to generate. Any dependencies will also be included recursively
+        /// <para>(e.g. if a property of a type is of some type not in the list, that type will be generated as well)</para>
+        /// </param>
+        /// <returns></returns>
+        public static string Generate(IEnumerable<Type> types, ClosureExternsOptions options)
+        {
+            var generator = new ClosureExternsGenerator(types, options);
+            return generator.Generate();
+        }
+
+
+        /// <summary>
+        /// Generates closure externs using the default options.
+        /// </summary>
+        /// <param name="types">A set of base types to generate. Any dependencies will also be included recursively
+        /// <para>(e.g. if a property of a type is of some type not in the list, that type will be generated as well)</para>
+        /// </param>
+        /// <returns></returns>
+        public static string Generate(IEnumerable<Type> types)
+        {
+            var generator = new ClosureExternsGenerator(types, new ClosureExternsOptions());
+            return generator.Generate();
+        }
+
+        /// <summary>
+        /// Utility. Returns all the types in the same assembly and namespace (or deeper) as the given example type.
+        /// </summary>
+        public static IEnumerable<Type> GetTypesInNamespace(Type exampleType)
+        {
+            var targetNamespace = exampleType.Namespace;
+            return exampleType.Assembly
+                              .GetTypes()
+                // Anonymous types have null namespace
+                              .Where(x => (null != x.Namespace) && x.Namespace.StartsWith(targetNamespace));
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected string Generate()
+        {
+            var resultBuilder = new StringBuilder();
+
+            var graph = new QuickGraph.AdjacencyGraph<Type, QuickGraph.Edge<Type>>(false);
+            graph.AddVertexRange(_types);
+
+            var typeDefinitions = new Dictionary<Type, String>();
+            resultBuilder.AppendLine("/** @const */");
+            resultBuilder.AppendLine("var " + this._options.NamespaceVarName + " = {};");
+            resultBuilder.AppendLine();
+
+            GenerateTypeDefinitions(this._options.NamespaceVarName, graph, typeDefinitions);
+
+            foreach (var type in graph.TopologicalSort().Reverse())
+            {
+                string definition;
+                if (typeDefinitions.TryGetValue(type, out definition))
+                {
+                    resultBuilder.Append(definition);
+                }
+                else if (false == type.Assembly.Equals(Assembly.GetAssembly(typeof(string))))
+                {
+                    resultBuilder.AppendLine("// unknown: " + type.Name + "\n");
+                }
+            }
+
+            return resultBuilder.ToString();
+        }
+
+        protected void GenerateTypeDefinitions(string closureNamespaceVar, QuickGraph.AdjacencyGraph<Type, QuickGraph.Edge<Type>> graph, Dictionary<Type, string> typeDefinitions)
+        {
+            // Every time we generate a type, we may find new type dependencies and they will be added to the graph.
+            // We keep generating types as long as the graph contains items that were not generated.
+            var processedTypes = new HashSet<Type>();
+            while (true)
+            {
+                var unprocessedTypes = graph.Vertices.Except(processedTypes);
+                if (false == unprocessedTypes.Any())
+                {
+                    break;
+                }
+                foreach (var type in unprocessedTypes)
+                {
+                    processedTypes.Add(type);
+                    if (false == InAllowedAssemblies(type))
+                    {
+                        continue;
+                    }
+                    GenerateTypeDefinition(closureNamespaceVar, graph, typeDefinitions, type);
+                }
+            }
+        }
+
+        private bool InAllowedAssemblies(Type type)
+        {
+            return this._assemblies.Any(x => x.Equals(type.Assembly));
+        }
+
+        protected void GenerateTypeDefinition(string closureNamespaceVar, QuickGraph.AdjacencyGraph<Type, QuickGraph.Edge<Type>> graph, Dictionary<Type, string> typeDefinitions, Type type)
+        {
+            if (type.IsClass)
+            {
+                var typeResultBuilder = GenerateClassDefinition(type, graph);
+                typeDefinitions.Add(type, typeResultBuilder.ToString());
+            }
+
+            if (type.IsEnum)
+            {
+                var typeResultBuilder = GenerateEnumDefinition(type, graph);
+                typeDefinitions.Add(type, typeResultBuilder.ToString());
+            }
+        }
+
+        protected Type MapType(Type type)
+        {
+            return this._options.MapType(type);
+        }
+
+        protected StringBuilder GenerateClassDefinition(Type type, QuickGraph.AdjacencyGraph<Type, QuickGraph.Edge<Type>> graph)
+        {
+            Console.Error.WriteLine("Generating Class: " + type.Name);
+            var typeResultBuilder = new StringBuilder();
+            var className = this.GetFullTypeName(type);
+            AppendTypeComment(typeResultBuilder, type);
+
+            typeResultBuilder.AppendLine("/** @constructor");
+            if (type.IsGenericTypeDefinition)
+            {
+                typeResultBuilder.AppendLine(" * @template " +
+                    String.Join(", ", type.GetGenericArguments().Select(x => x.Name)));
+            }
+            foreach (var annotation in this._options.ConstructorAnnotations(className))
+            {
+                typeResultBuilder.AppendLine(" * " + String.Format(annotation, className));
+            }
+            typeResultBuilder.AppendLine(" */");
+            typeResultBuilder.AppendLine(className + " = " + this._options.ConstructorExpression(className) + ";");
+
+            var properties = type.GetProperties();
+            foreach (var propertyPair in WithIsLast(properties))
+            {
+                var property = propertyPair.Item1;
+                var isLast = propertyPair.Item2;
+                string propertyName = property.Name.Substring(0, 1).ToLowerInvariant() + property.Name.Substring(1);
+                var mappedType = MapType(property.PropertyType);
+                var jsTypeName = GetJSTypeName(type, mappedType, graph);
+                typeResultBuilder.AppendLine(String.Format("/** @type {{{0}}} */", jsTypeName));
+                typeResultBuilder.AppendLine(String.Format("{0}.prototype.{1} = {2};", className, propertyName, GetDefaultJSValue(mappedType)));
+            }
+
+            typeResultBuilder.AppendLine();
+            return typeResultBuilder;
+        }
+
+        private object GetDefaultJSValue(Type type)
+        {
+            if (type.Equals(typeof(string)))
+            {
+                return "''";
+            }
+            if (type.Equals(typeof(bool)))
+            {
+                return "false";
+            }
+            if (type.IsEnum)
+            {
+                return this.GetFullTypeName(type) + "." + Enum.GetName(type, Activator.CreateInstance(type));
+            }
+            if (type.IsValueType && (false == type.Equals(typeof(DateTime))))
+            {
+                return Activator.CreateInstance(type);
+            }
+            return "null";
+        }
+
+        protected static void AppendTypeComment(StringBuilder typeResultBuilder, Type type)
+        {
+            typeResultBuilder.AppendLine("// " + type.FullName);
+        }
+
+        protected static IEnumerable<Tuple<T, bool>> WithIsLast<T>(T[] items)
+        {
+            return items.Select((p, i) => new Tuple<T, bool>(p, i + 1 == items.Count()));
+        }
+
+        protected string GetJSTypeName(Type sourceType, Type propertyType, QuickGraph.AdjacencyGraph<Type, QuickGraph.Edge<Type>> graph)
+        {
+            if (propertyType.IsGenericParameter)
+            {
+                return propertyType.Name;
+            }
+            var overrideTypeName = this._options.TryGetTypeName(propertyType);
+            if (false == String.IsNullOrWhiteSpace(overrideTypeName))
+            {
+                return overrideTypeName;
+            }
+            if (propertyType.IsGenericType)
+            {
+                var enumerableType = propertyType.GetInterfaces().SingleOrDefault(x => x.IsGenericType && x.GetGenericTypeDefinition().Equals(typeof(IEnumerable<>)));
+                if (null != enumerableType)
+                {
+                    return GetJSArrayTypeName(sourceType, enumerableType.GetGenericArguments().Single(), graph);
+                }
+            }
+            if (propertyType.IsArray)
+            {
+                var elementType = propertyType.GetElementType();
+                return GetJSArrayTypeName(sourceType, elementType, graph);
+            }
+            graph.AddVertex(propertyType);
+            graph.AddVertex(sourceType);
+            if (InAllowedAssemblies(propertyType))
+            {
+                graph.AddEdge(new QuickGraph.Edge<Type>(sourceType, propertyType));
+                return this.GetFullTypeName(propertyType);
+            }
+            return GetTypeName(propertyType);
+        }
+
+        private string GetJSArrayTypeName(Type sourceType, Type propertyType, QuickGraph.AdjacencyGraph<Type, QuickGraph.Edge<Type>> graph)
+        {
+            return "Array.<" + GetJSTypeName(sourceType, propertyType, graph) + ">";
+        }
+
+        protected string GetTypeName(Type type)
+        {
+            // Strip `1 (or `2, etc.) suffix from generic types
+            var typeName = new String(type.Name.TakeWhile(Char.IsLetterOrDigit).ToArray());
+            if (InAllowedAssemblies(type))
+            {
+                if (false == String.IsNullOrWhiteSpace(this._options.SuffixToTrimFromTypeNames))
+                {
+                    return TrimSuffix(typeName, this._options.SuffixToTrimFromTypeNames);
+                }
+                return typeName;
+            }
+            switch (typeName.ToLowerInvariant())
+            {
+                case "string": return "string";
+                case "boolean": return "boolean";
+                case "decimal": return "number";
+                case "double": return "number";
+                case "float": return "number";
+                case "int32": return "number";
+                case "datetime": return "Date";
+                default: return "?";
+            }
+        }
+
+        private static string TrimSuffix(string stringToTrim, string suffix)
+        {
+            if (stringToTrim.EndsWith(suffix, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return stringToTrim.Substring(0, stringToTrim.Length - suffix.Length);
+            }
+            return stringToTrim;
+        }
+
+        protected StringBuilder GenerateEnumDefinition(Type type, QuickGraph.AdjacencyGraph<Type, QuickGraph.Edge<Type>> graph)
+        {
+            Console.Error.WriteLine("Generating Enum: " + type.Name);
+            var typeResultBuilder = new StringBuilder();
+            AppendTypeComment(typeResultBuilder, type);
+            typeResultBuilder.AppendLine("/** @enum {string} */");
+            typeResultBuilder.AppendLine(GetFullTypeName(type) + " = {");
+            foreach (var pair in WithIsLast(Enum.GetNames(type)))
+            {
+                typeResultBuilder.AppendFormat("    {0}: '{0}'{1}\n", pair.Item1, pair.Item2 ? String.Empty : ",");
+            }
+            typeResultBuilder.AppendLine("};");
+            typeResultBuilder.AppendLine();
+            return typeResultBuilder;
+        }
+
+        private string GetFullTypeName(Type type)
+        {
+            return this._options.NamespaceVarName + "." + GetTypeName(type);
+        }
+
+        #endregion
+    }
+}
---
+++ ClosureExterns/ClosureExternsOptions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BDBPayroll.Billing.API.Web.JSCodeGenerator
+{
+    /// <summary>
+    /// Represents a set of options to use when generating closure annotations (externs).
+    /// The default constructor initializes all properties to default values.
+    /// <para>See each property's doc for details and default values.</para>
+    /// </summary>
+    public class ClosureExternsOptions
+    {
+        public ClosureExternsOptions()
+        {
+            this.NamespaceVarName = "Types";
+            this.ConstructorAnnotations = x => new string[0];
+            this.SuffixToTrimFromTypeNames = "dto";
+            this.ConstructorExpression = x => "function() {}";
+            this.MapType = x => x;
+            this.TryGetTypeName = x => null;
+        }
+
+        /// <summary>
+        /// The name to use for the javascript variable which will contain all the types.
+        /// <para>Default is: "Types"</para>
+        /// </summary>
+        public string NamespaceVarName { get; set; }
+
+        /// <summary>
+        /// An expression to use for when defining constructors. Useful for creating derived types.
+        /// <para>The parameter is the closure type name of the current class (so you can use it in the output you generate)</para>
+        /// <para>Default is: x => "function() {}"</para>
+        /// </summary>
+        public Func<string, string> ConstructorExpression { get; set; }
+
+        /// <summary>
+        /// Additional annotations to apply to constructors. For example: ["@struct", "@final"]
+        /// <para>The parameter is the closure type name of the current class (so you can use it in the output you generate)</para>
+        /// <para>Default is: [] (empty array)</para>
+        /// </summary>
+        public Func<string, string[]> ConstructorAnnotations { get; set; }
+
+        /// <summary>
+        /// A string that should be trimmed from the end of all type names, case-insensitive. 
+        /// <para>(e.g. "dto" will caused "MyDTO" to be trimmed to "My")</para>
+        /// <para>Default is: "dto"</para>
+        /// </summary>
+        public string SuffixToTrimFromTypeNames { get; set; }
+
+        /// <summary>
+        /// A function to map input types to actual types to be outputted. Useful for converting some property types of an object
+        /// into something else (e.g. your special ID type into a string) that represents how it will actually be serialized to 
+        /// json.
+        /// <para>Default is: x => x</para>
+        /// </summary>
+        public Func<Type, Type> MapType { get; set; }
+
+        /// <summary>
+        /// A function that optionally generates a specific closure type expression for the given type.
+        /// <para>Returns null to signify that there is no special substitution, and that the generator should
+        /// find the appropriate type expression.</para>
+        /// <para>Default is: x => null</para>
+        /// </summary>
+        public Func<Type, string> TryGetTypeName { get; set; } 
+    }
+}
